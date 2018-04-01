221d8e0308934c901eb2cd399fce9b71839878b6
Nick Rybak <nrybak94@gmail.com>
2018-04-01 10:23:49 +0300

    task#2: add list of possible atack for People warrior

--- src/main/java/io/rybak/heroe/people/warrior/FightAbility.java
+++ src/main/java/io/rybak/heroe/people/warrior/FightAbility.java
@@ -1,4 +1,18 @@
 package io.rybak.heroe.people.warrior;
 
+import lombok.Getter;
+
 public enum FightAbility {
+    ATTACK("attack with a sword", 11);
+
+    @Getter
+    private String name;
+
+    @Getter
+    private int damage;
+
+    FightAbility(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
 }
